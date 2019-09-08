1765b087d36e22cd7e7eb4383a4ef27c0218e6b5
Alfhilld <g.jernigan5270@gmail.com>
2019-09-08 00:29:41 -0400

    Server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-//mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
